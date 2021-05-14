15cbbaa2b50d4ecaf9b79166e50cb98b825e7e3f
titulebolide <seneztit@gmail.com>
2021-05-14 16:54:52 +0200

    add miner

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import conf
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+## CRYPTO REPORT
+
 sheet = gspread.service_account(
     filename=conf.GOOGLE_TOKEN_FILE
 ).open_by_key(conf.SHEET_ID).get_worksheet(0)
@@ -36,7 +38,7 @@ for col in range(2, sheet_data.shape[1]):
 total = sum(symbol_eur_value.values())
 profits = total + float(sheet_data[2,1])
 
-msg = "*** \n### Compte rendu cryptos 📈 : \n *** \n"
+msg = "*** \n### Compte rendu cryptos 📈 : \n*** \n#### Marché actuel:"
 for symbol, value in symbol_eur_value.items():
     msg += "- **{}** *({} €)* : {} €\n".format(symbol, symbol_ticker[symbol], round(value,2))
 
@@ -46,7 +48,24 @@ if profits > 0:
     color = "#9dc209"
 else:
     color = "#b22222"
-msg+="**Total** : {}€ <br/> <font color='{}'>**Profit** : {} €</font> \n ***".format(round(total,2), color, round(profits,2))
+msg+="**Total** : {}€ <br/> <font color='{}'>**Profit** : {} €</font> \n***\n".format(round(total,2), color, round(profits,2))
 
+## MINER REPORT
 
-requests.post("http://127.0.0.1:"+str(conf.BOT_PORT), data={"msg":msg})
+miner_data = requests.get(
+    'https://api.ethermine.org/miner/'+str(conf.MINER_ADRESS)+'/dashboard'
+).json()['data']
+
+unpaid = miner_data['currentStatistics']['unpaid']/1e18
+unpaid_eur = unpaid*getSymbolTicker('ETH')
+
+msg += "#### Mineur"
+msg += "\n- **Actif** : "+ 'Oui' if miner_data['currentStatistics']['activeWorkers'] else 'Non'
+msg += "\n- **Non payé** : {} ETH *({} €)*".format(unpaid, unpaid_eur)
+msg += "\n***"
+
+## SEND DATA
+try:
+    requests.post("http://127.0.0.1:"+str(conf.BOT_PORT), data={"msg":msg})
+except requests.exceptions.ConnectionError:
+    print(msg)
