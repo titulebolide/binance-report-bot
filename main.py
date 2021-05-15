d4e2fa69d4eb077cb3e36f4774c8e0dc48dcb134
titulebolide <seneztit@gmail.com>
2021-05-16 01:44:33 +0200

    add the otpion of sending the message or simply store the report

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import bot
+import click
 
-st = bot.utils.SymbolTicker()
-crypto_report = bot.crypto.get_report(st)
-miner_report = bot.miner.get_report(st)
+@click.command()
+@click.option('--send/--no-send', default=False)
+def main(send):
+    st = bot.utils.SymbolTicker()
+    crypto_report = bot.crypto.get_report(st)
+    miner_report = bot.miner.get_report(st)
 
-crypto_reports = bot.crypto.save_report(crypto_report, bot.crypto.get_previous_reports())
-figname = bot.crypto.plot_reports(crypto_reports)
+    crypto_reports = bot.crypto.save_report(crypto_report, bot.crypto.get_previous_reports())
+    figname = bot.crypto.plot_reports(crypto_reports)
 
-msg = "*** \n### Compte rendu cryptos 📈 : \n***\n\n"
-msg += bot.crypto.format_report(st,crypto_report)
-msg += bot.miner.format_report(st,miner_report)
+    msg = "*** \n### Compte rendu cryptos 📈 : \n***\n\n"
+    msg += bot.crypto.format_report(st,crypto_report)
+    msg += bot.miner.format_report(st,miner_report)
 
-bot.utils.send_report(msg)
-bot.utils.send_img(figname)
+    if send:
+        bot.utils.send_report(msg)
+        bot.utils.send_img(figname)
+
+if __name__ == "__main__":
+    main()
