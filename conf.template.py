cfa4d619accdb5fe5a6b4e7da1a07ec93af03605
titulebolide <seneztit@gmail.com>
2021-06-03 08:51:22 +0200

    add documentation to conf

--- conf.template.py
+++ conf.template.py
@@ -1,6 +1,19 @@
+# Your binance API keys
+# ATTENTION : CENSOR THESE KEYS IF ASKED TO PASTE THIS FILE
 BINANCE_API_KEY = ""
 BINANCE_API_SECRET = ""
+
+# The list of coins that you would like to follow
 COINS = ["BTC","ADA","ATOM","BAT","BTT","CAKE","DASH","EOS","ETC","ICX","IOTA","NEO","OMG","ONT","QTUM","ROSE","TRX","VET","WIN","XLM"]
+
+# The currency with which the reports will be printed
 CURRENCY = "EUR" #Or USD
+
+# The symbol of your currency
 CURRENCY_SYMBOL = "€"
+
+#Set this to false if your terminal does not support rich printing
 RICH_PRINTING = True
+
+#Leave blank if you don't want to use apprise notifications
+APPRISE_URL = ""
